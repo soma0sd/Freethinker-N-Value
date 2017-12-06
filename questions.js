3a86b6ced1b600cb26e7541874b819276dfcab82
DESKTOP-K2909O4\soma <soma0sd@gmail.com>
2017-12-07 00:38:13 +0900

    퀴즈 테스트 2차시기

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "나는 만쥐작이 너무 좋다",
         "effect": {
             "val1": 10,
             "val2": 0,
@@ -9,11 +9,38 @@ questions = [
         }
     },
     {
-        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "question": "뚜껑을 따는 일은 항상 즐겁다",
         "effect": {
           "val1": 10,
           "val2": 0,
           "val3": 0,
+          "val4": 10
+        }
+    },
+    {
+        "question": "나는 예쁘다",
+        "effect": {
+          "val1": 0,
+          "val2": 10,
+          "val3": 0,
+          "val4": 0
+        }
+    },
+    {
+        "question": "나는 귀엽다",
+        "effect": {
+          "val1": 0,
+          "val2": 10,
+          "val3": 10,
+          "val4": 0
+        }
+    },
+    {
+        "question": "필수 3대 영양소인 니코틴, 알코올, 카페인 중 알코올이 최고다",
+        "effect": {
+          "val1": 0,
+          "val2": 0,
+          "val3": 10,
           "val4": 0
         }
     }
