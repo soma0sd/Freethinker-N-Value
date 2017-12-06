77bece2d397e5781ce4bb3040535786f77b06634
DESKTOP-K2909O4\soma <soma0sd@gmail.com>
2017-12-07 00:29:12 +0900

    퀴즈 테스트

---
+++ questions.js
@@ -0,0 +1,20 @@
+questions = [
+    {
+        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "effect": {
+            "val1": 10,
+            "val2": 0,
+            "val3": 0,
+            "val4": 0
+        }
+    },
+    {
+        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "effect": {
+          "val1": 10,
+          "val2": 0,
+          "val3": 0,
+          "val4": 0
+        }
+    }
+];
