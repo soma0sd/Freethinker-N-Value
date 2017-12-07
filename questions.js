fe33febd44ed97804e3a1d7cac6c18b9ee2e98b3
DESKTOP-K2909O4\soma <soma0sd@gmail.com>
2017-12-07 13:59:54 +0900

    퀴즈 함수 일반화 및 j쿼리로 교체

--- questions.js
+++ questions.js
@@ -1,47 +1,22 @@
 questions = [
     {
         "question": "나는 만쥐작이 너무 좋다",
-        "effect": {
-            "val1": 10,
-            "val2": 0,
-            "val3": 0,
-            "val4": 0
-        }
+        "effect": [10, 0, 0, 0]
     },
     {
         "question": "뚜껑을 따는 일은 항상 즐겁다",
-        "effect": {
-          "val1": 10,
-          "val2": 0,
-          "val3": 0,
-          "val4": 10
-        }
+        "effect": [10, 0, 0, 0]
     },
     {
         "question": "나는 예쁘다",
-        "effect": {
-          "val1": 0,
-          "val2": 10,
-          "val3": 0,
-          "val4": 0
-        }
+        "effect": [0, 10, 0, 0]
     },
     {
         "question": "나는 귀엽다",
-        "effect": {
-          "val1": 0,
-          "val2": 10,
-          "val3": 10,
-          "val4": 0
-        }
+        "effect": [0, 10, 10, 0]
     },
     {
         "question": "필수 3대 영양소인 니코틴, 알코올, 카페인 중 알코올이 최고다",
-        "effect": {
-          "val1": 0,
-          "val2": 0,
-          "val3": 10,
-          "val4": 0
-        }
+        "effect": [10, 0, 0, 10]
     }
 ];
