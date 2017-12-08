ed8972d19dba5dca70c9aa91620c9b1d3d7e1a06
DESKTOP-K2909O4\soma <soma0sd@gmail.com>
2017-12-09 02:06:09 +0900

    FB 2차 시도

--- js/result.js
+++ js/result.js
@@ -88,6 +88,22 @@ function canvasControl(){
   }
 }
 function init_FB(){
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '161236511051720',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
   if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
       XMLHttpRequest.prototype.sendAsBinary = function(string) {
           var bytes = Array.prototype.map.call(string, function(c) {
