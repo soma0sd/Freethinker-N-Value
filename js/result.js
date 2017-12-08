546f62b064609afba2c37333553c2658d63ecfff
DESKTOP-K2909O4\soma <soma0sd@gmail.com>
2017-12-09 02:26:52 +0900

    FB 4차 시도

--- js/result.js
+++ js/result.js
@@ -96,29 +96,29 @@ function init_FB(){
   //     version          : 'v2.11'
   //   });
   // };
-  if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
-      XMLHttpRequest.prototype.sendAsBinary = function(string) {
-          var bytes = Array.prototype.map.call(string, function(c) {
-              return c.charCodeAt(0) & 0xff;
-          });
-          this.send(new Uint8Array(bytes).buffer);
-      };
-  };
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  window.fbAsyncInit = function() {
-      FB.init({
-        appId  : "404477149649756",
-        status : true,
-        cookie : true,
-        xfbml  : true  // parse XFBML
-      });
-  };
+  // if ( XMLHttpRequest.prototype.sendAsBinary === undefined ) {
+  //     XMLHttpRequest.prototype.sendAsBinary = function(string) {
+  //         var bytes = Array.prototype.map.call(string, function(c) {
+  //             return c.charCodeAt(0) & 0xff;
+  //         });
+  //         this.send(new Uint8Array(bytes).buffer);
+  //     };
+  // };
+  // (function(d, s, id) {
+  //   var js, fjs = d.getElementsByTagName(s)[0];
+  //   if (d.getElementById(id)) return;
+  //   js = d.createElement(s); js.id = id;
+  //   js.src = "//connect.facebook.net/en_US/all.js";
+  //   fjs.parentNode.insertBefore(js, fjs);
+  // }(document, 'script', 'facebook-jssdk'));
+  // window.fbAsyncInit = function() {
+  //     FB.init({
+  //       appId  : "404477149649756",
+  //       status : true,
+  //       cookie : true,
+  //       xfbml  : true  // parse XFBML
+  //     });
+  // };
 }
 function postImageToFacebook( authToken, filename, mimeType, imageData, message ){
     // this is the multipart/form-data boundary we'll use
