a62b12f1f0fe10da1227cecb37fc4ea07fb35830
DESKTOP-K2909O4\soma <soma0sd@gmail.com>
2017-12-09 02:54:56 +0900

    FB SDK 변경

--- js/result.js
+++ js/result.js
@@ -87,52 +87,7 @@ function canvasControl(){
     ctx.fillText(values_label[i],arg[0],arg[1]-arg[3]*0.09);
   }
 }
-function init_FB(){}
-function postImageToFacebook( authToken, filename, mimeType, imageData, message ){
-    // this is the multipart/form-data boundary we'll use
-    var boundary = '----ThisIsTheBoundary1234567890';
-    // let's encode our image file, which is contained in the var
-    var formData = '--' + boundary + '\r\n'
-    formData += 'Content-Disposition: form-data; name="source"; filename="' + filename + '"\r\n';
-    formData += 'Content-Type: ' + mimeType + '\r\n\r\n';
-    for ( var i = 0; i < imageData.length; ++i )
-    {
-        formData += String.fromCharCode( imageData[ i ] & 0xff );
-    }
-    formData += '\r\n';
-    formData += '--' + boundary + '\r\n';
-    formData += 'Content-Disposition: form-data; name="message"\r\n\r\n';
-    formData += message + '\r\n'
-    formData += '--' + boundary + '--\r\n';
-
-    var xhr = new XMLHttpRequest();
-    xhr.open( 'POST', 'https://graph.facebook.com/me/photos?access_token=' + authToken, true );
-    xhr.onload = xhr.onerror = function() {
-        console.log( xhr.responseText );
-    };
-    xhr.setRequestHeader( "Content-Type", "multipart/form-data; boundary=" + boundary );
-    xhr.sendAsBinary( formData );
-};
-
 //외부함수
-function postCanvasToFacebook() {
-	var data = canvas.toDataURL("image/png");
-	var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
-	var decodedPng = Base64Binary.decode(encodedPng);
-	FB.getLoginStatus(function(response) {
-	  if (response.status === "connected") {
-		postImageToFacebook(response.authResponse.accessToken, "heroesgenerator", "image/png", decodedPng, "www.heroesgenerator.com");
-	  } else if (response.status === "not_authorized") {
-		 FB.login(function(response) {
-			postImageToFacebook(response.authResponse.accessToken, "heroesgenerator", "image/png", decodedPng, "www.heroesgenerator.com");
-		 }, {scope: "publish_actions"});
-	  } else {
-		 FB.login(function(response)  {
-			postImageToFacebook(response.authResponse.accessToken, "heroesgenerator", "image/png", decodedPng, "www.heroesgenerator.com");
-		 }, {scope: "publish_actions"});
-	  }
-	 });
-};
 $("#result-box").ready(function(){
   canvasControl();
 })
